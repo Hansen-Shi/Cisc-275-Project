69872359dbb85f928114a8f8a3d104e7cec31ab8
Jake Screen <jscreen@udel.edu>
2019-12-01 21:22:30 -0500

    nuked

--- Direction.java
+++
@@ -1,5 +0,0 @@
-
-
-public enum Direction {
-	NORTH, SOUTH, WEST, EAST, NORTHEAST, NORTHWEST, SOUTHEAST, SOUTHWEST;
-}
--- Level.java
+++
@@ -1,4 +0,0 @@
-
-public enum Level {
-	pvzPipe, animalPhoto, netGame, opening;
-}
